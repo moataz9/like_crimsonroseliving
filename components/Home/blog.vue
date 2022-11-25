cfb17da60e1e3a3867dee29c63f23d1afbe25f40
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-11-25 10:37:04 +0200

    blog animation with gsap

--- components/Home/blog.vue
+++ components/Home/blog.vue
@@ -1,13 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { $use_gsap } = useNuxtApp()
+onMounted(() => {
+  $use_gsap.gsap.to('.blog-card', {
+    y: 0,
+    duration: 2,
+    stagger: 0.3,
+    transformOrigin: 'left',
+    scrollTrigger: {
+      trigger: '.blog',
+      toggleActions: 'restart pause restart none',
+    },
+  })
+})
+</script>
 
 <template>
-  <div class="bg-gray-200 mt-48">
+  <div class="blog bg-gray-200 mt-48">
     <section class="2xl:container mx-auto my-36 xl:px-32 px-3">
       <div class="flex flex-wrap justify-between max-xl:justify-center">
-        <BlogCard v-for="item in 3" :key="item"></BlogCard>
+        <BlogCard class="blog-card" v-for="item in 3" :key="item"></BlogCard>
       </div>
     </section>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.blog-card {
+  @apply translate-y-96
+}
+</style>
