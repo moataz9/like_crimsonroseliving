849f89603816791deea500af7a2814dc5525dc2f
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-11-25 10:37:30 +0200

    choose animatin with gsap

--- components/Home/ChooseUs.vue
+++ components/Home/ChooseUs.vue
@@ -1,12 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { $use_gsap } = useNuxtApp()
+onMounted(() => {
+  $use_gsap.gsap.to('.choose_us h2, .choose_us-card, .choose_us-desc', {
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    stagger: 0.3,
+    scrollTrigger: {
+      trigger: '.choose_us',
+      toggleActions: 'restart pause restart none',
+    },
+  })
+})
+</script>
 
 <template>
-  <section class="2xl:container mx-auto my-36">
+  <section class="choose_us 2xl:container mx-auto my-36">
     <h2 class="text-3xl capitalize text-center">
       Why Choose Us <br />
       <span class="bg-red-900 h-1 rounded-sm w-[60px] block mx-auto mt-3"></span>
     </h2>
-    <div class="capitalize text-gray-500 xl:px-96 px-14 text-justify">
+    <div class="choose_us-desc capitalize text-gray-500 xl:px-96 px-14 text-justify">
       <p class="mt-6">
         With a team of highly skilled employees and management whose focus is laser-sharp on their work,
         we provide clients with convenience and confidence throughout the life of the project and for
@@ -14,7 +28,7 @@
       </p>
     </div>
     <div class="flex flex-wrap justify-between max-md:justify-center my-12 xl:px-32 px-8">
-      <div class="max-w-[50%] lg:max-w-xs flex flex-col items-center px-8 mt-8">
+      <div class="choose_us-card max-w-[50%] lg:max-w-xs flex flex-col items-center px-8 mt-8">
         <span class="inline-block p-8 border-2 border-red-600 rounded-full icon-hover">
           <Icon name="fa:handshake-o" size="80" class="text-red-600" />
         </span>
@@ -25,7 +39,7 @@
           achievements.
         </p>
       </div>
-      <div class="max-w-[50%] lg:max-w-xs flex flex-col items-center px-8 mt-8">
+      <div class="choose_us-card max-w-[50%] lg:max-w-xs flex flex-col items-center px-8 mt-8">
         <span class="inline-block p-8 border-2 border-red-600 rounded-full icon-hover">
           <Icon name="fa:check-square-o" size="80" class="text-red-600" />
         </span>
@@ -36,7 +50,7 @@
           to work on several exclusive projects.
         </p>
       </div>
-      <div class="max-w-[50%] lg:max-w-xs flex flex-col items-center px-8 mt-8">
+      <div class="choose_us-card max-w-[50%] lg:max-w-xs flex flex-col items-center px-8 mt-8">
         <span class="inline-block p-8 border-2 border-red-600 rounded-full icon-hover">
           <Icon name="fa:cogs" size="80" class="text-red-600" />
         </span>
@@ -47,7 +61,7 @@
           deliver the best service possible.
         </p>
       </div>
-      <div class="max-w-[50%] lg:max-w-xs flex flex-col items-center px-8 mt-8">
+      <div class="choose_us-card max-w-[50%] lg:max-w-xs flex flex-col items-center px-8 mt-8">
         <span class="inline-block p-8 border-2 border-red-600 rounded-full icon-hover">
           <Icon name="fa:home" size="80" class="text-red-600" />
         </span>
@@ -70,6 +84,14 @@
     .icon {
       @apply text-white;
     }
+  }
+}
+.choose_us {
+  &-card,
+  &-desc,
+  h2 {
+    transform: translateY(-100px);
+    opacity: 0;
   }
 }
 </style>
